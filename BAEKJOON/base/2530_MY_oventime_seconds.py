d2a5bc1c7b7831905887e3ffc2931ee6bfb233d6
hellosonic-r <hellommmh@gmail.com>
2023-02-10 23:55:05 +0900

    baekjoon

---
+++ BAEKJOON/base/2530_MY_oventime_seconds.py
@@ -0,0 +1,13 @@
+hour, minute, second = map(int, input().split())
+time = int(input())
+
+result_second = (time + second) % 60
+result_minute = minute + (second + time) // 60
+result_hour = hour + (minute + (second + time) // 60) // 60
+
+if result_hour >= 24:
+    result_hour -= 24
+if result_minute >= 60:
+    result_minute = result_minute % 60
+
+print('{} {} {}'.format(result_hour, result_minute, result_second))
