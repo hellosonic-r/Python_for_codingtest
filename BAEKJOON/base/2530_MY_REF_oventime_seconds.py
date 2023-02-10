cfefd28211322e9a64339d5b69a01cb3fc340eff
hellosonic-r <hellommmh@gmail.com>
2023-02-11 00:07:19 +0900

    baekjoon

--- BAEKJOON/base/2530_MY_REF_oventime_seconds.py
+++ BAEKJOON/base/2530_MY_REF_oventime_seconds.py
@@ -6,7 +6,7 @@ result_minute = minute + (second + time) // 60
 result_hour = hour + (minute + (second + time) // 60) // 60
 
 if result_hour >= 24:
-    result_hour -= 24
+    result_hour = result_hour % 24 #이 부분 ref 참고하여 수정함
 if result_minute >= 60:
     result_minute = result_minute % 60
 
