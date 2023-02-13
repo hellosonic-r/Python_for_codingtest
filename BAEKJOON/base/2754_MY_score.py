ac72a17fab31a2070f8396b695b76a0404024ec8
hellosonic-r <hellommmh@gmail.com>
2023-02-13 17:55:03 +0900

    baekjoon

---
+++ BAEKJOON/base/2754_MY_score.py
@@ -0,0 +1,28 @@
+C_languange = input()
+
+if C_languange == "A+":
+    print(4.3)
+elif C_languange == "A0":
+    print(4.0)
+elif C_languange == "A-":
+    print(3.7)
+elif C_languange == "B+":
+    print(3.3)
+elif C_languange == "B0":
+    print(3.0)
+elif C_languange == "B-":
+    print(2.7)
+elif C_languange == "C+":
+    print(2.3)
+elif C_languange == "C0":
+    print(2.0)   
+elif C_languange == "C-":
+    print(1.7)
+elif C_languange == "D+":
+    print(1.3)
+elif C_languange == "D0":
+    print(1.0)   
+elif C_languange == "D-":
+    print(0.7)
+elif C_languange == "F":
+    print(0.0)
