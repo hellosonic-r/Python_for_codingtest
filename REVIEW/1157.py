6f0fd7c356b04ebda2cefa956526242ad264d06b
hellosonic-r <hellommmh@gmail.com>
2023-03-01 18:10:01 +0900

    review

---
+++ REVIEW/1157.py
@@ -0,0 +1,19 @@
+string = list(input().upper())
+
+
+string_list = list(set(list(map(str, str(string)))))
+cnt = []
+for i in range(len(string_list)):
+    cnt.append(string.count(string_list[i]))
+
+if cnt.count(max(cnt)) >= 2:
+    print("?")
+else:
+    print(string_list[cnt.index(max(cnt))]) 
+
+# string = input().upper()
+
+# print(string)
+
+# cnt = string.count("C")
+# print(cnt)
---
+++ REVIEW/2164.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+n = int(input())
+card = []
+for i in range(1,n+1):
+    card.append(i)
+queue = deque(card)
+
+while len(queue) != 1:
+    queue.popleft()
+    queue.append(queue.popleft())
+
+print(queue[0])
+
