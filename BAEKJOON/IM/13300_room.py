db733982efd6254f179efb7a44a61e0915f3fe0a
hellosonic-r <hellommmh@gmail.com>
2023-03-18 10:12:20 +0900

    baekjoon/bronze2

--- BAEKJOON/IM/13300_room.py
+++ BAEKJOON/IM/13300_room.py
@@ -1,40 +1,23 @@
 n, k = map(int, input().split())
-grade = [[0]*2 for _ in range(6)]
+room_boy = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+room_girl = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
 for _ in range(n):
     s, y = map(int, input().split())
     if s == 1:
-        if y == 1:
-            grade[0][0] += 1
-        elif y == 2:
-            grade[1][0] += 1
-        elif y == 3:
-            grade[2][0] += 1
-        elif y == 4:
-            grade[3][0] += 1
-        elif y == 5:
-            grade[4][0] += 1
-        elif y == 6:
+        room_boy[y] += 1
+    else:
+        room_girl[y] += 1
 
-            grade[5][0] += 1
-    if s == 0:
-        if y == 1:
-            grade[0][1] += 1
-        elif y == 2:
-            grade[1][1] += 1
-        elif y == 3:
-            grade[2][1] += 1
-        elif y == 4:
-            grade[3][1] += 1
-        elif y == 5:
-            grade[4][1] += 1
-        elif y == 6:
-            grade[5][1] += 1
-count = 12
+cnt = 0
+for i in range(1,7):
+    if room_boy[i] % k == 0:
+        cnt += room_boy[i]//k
+    else:
+        cnt += (room_boy[i]//k) +1
+    if room_girl[i] % k == 0:
+        cnt += room_girl[i]//k
+    else:
+        cnt += (room_girl[i]//k) +1
 
-for i in range(6):
-    for j in range(2):
-        if grade[i][j] > k:
-            count += grade[i][j] // k
-        if grade[i][j] == 0:
-            count -= 1
-print(count)
+print(cnt)
+
