bfeeb56c822bc8fba4779572c3854333d447d082
hellosonic-r <hellommmh@gmail.com>
2023-03-23 14:40:37 +0900

    baekjoon/bronze1

---
+++ REVIEW/14696.py
@@ -0,0 +1,31 @@
+n = int(input())
+
+for _ in range(n):
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    a.pop(0)
+    a.sort(reverse=True)
+    b.pop(0)
+    b.sort(reverse=True)
+    if a>b:
+        print("A")
+    elif a<b:
+        print("B")
+    else:
+        print("D")
+
+# a = [4,4,4]
+# b = [4,3,2,1,1]
+
+# print(a>b)
+
+# a = [4,4,4]
+# b = [4,3,4,4,4]
+
+# print(a>b)
+
+a = ['apple','banana','coconut']
+b = ['apple','bbbbbb']
+
+print(a<b)
+
