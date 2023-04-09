4574f8c54872e79bb4049edf615e13d1d15c6093
hellosonic-r <hellommmh@gmail.com>
2023-04-09 17:55:41 +0900

    baekjoon/bronze2

---
+++ REVIEW/1592_2.py
@@ -0,0 +1,18 @@
+import sys
+
+n,m,l = map(int, sys.stdin.readline().split())
+
+arr=[0]*n # 1 0 0 0 0 
+arr[0] += 1
+count = 0
+ball_idx = 0 
+while max(arr) < m:
+    if arr[ball_idx] % 2 == 1:
+        arr[(ball_idx+l)%n] += 1
+        ball_idx = (ball_idx+l)%n
+    elif arr[ball_idx] % 2 == 0:
+        arr[(ball_idx-l+n)%n] += 1
+        ball_idx = (ball_idx-l+n)%n
+    count += 1
+
+print(count)
