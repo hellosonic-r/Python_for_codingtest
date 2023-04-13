6dd1e2029bd6329b0837deca5b2290e7b35db60a
hellosonic-r <hellommmh@gmail.com>
2023-04-13 11:40:55 +0900

    baekjoon/silver3

--- BAEKJOON/Silver/15650_NM_2.py
+++ BAEKJOON/Silver/15650_NM_2.py
@@ -1,27 +1,62 @@
-s = []
+# s = []
 
-def check_sort(num_list):
-    for i in range(len(num_list)-1):
-        if num_list[i+1] - num_list[i] > 0:
-            continue
-        else:
-            return False
-    return True
+# def check_sort(num_list):
+#     for i in range(len(num_list)-1):
+#         if num_list[i+1] - num_list[i] > 0:
+#             continue
+#         else:
+#             return False
+#     return True
 
-def dfs():
-    if len(s) == m:
-        if check_sort(s):
-            print(" ".join(map(str, s)))
-            return
-        else:
-            return 
+# def dfs():
+#     if len(s) == m:
+#         if check_sort(s):
+#             print(" ".join(map(str, s)))
+#             return
+#         else:
+#             return 
 
-    for i in range(1, n+1):
-        if i not in s:
-            s.append(i)
-            dfs()
-            s.pop()
+#     for i in range(1, n+1):
+#         if i not in s:
+#             s.append(i)
+#             dfs()
+#             s.pop()
 
+# n, m = map(int, input().split())
+
+# dfs()
+
+### 백트래킹 연습
+# 1. 이진트리
+
+# def dfs(selc_num, temp_list):
+#     if selc_num > n:
+#         if len(temp_list) == m:
+#             ans.append(temp_list)
+#         return
+#     dfs(selc_num+1, temp_list+[selc_num]) #선택
+#     dfs(selc_num+1, temp_list) #선택x 
+
+# n, m = map(int, input().split())
+# ans = []
+
+# dfs(1, [])
+# for i in ans:
+#     print(*i)
+
+
+# 2. 멀티트리
+
+def dfs(index, s, temp_list):
+    if index == m:
+        ans.append(temp_list)
+        return
+    
+    for i in range(s, n+1):
+        dfs(index+1, i+1, temp_list+[i])
+        
 n, m = map(int, input().split())
+ans = []
 
-dfs()
+dfs(0, 1, [])
+print(ans)
--- BAEKJOON/Silver/15651_NM_3.py
+++ BAEKJOON/Silver/15651_NM_3.py
@@ -1,18 +1,50 @@
-ans = []
+# ans = []
 
-def dfs(first):
-    if len(ans) == m:
+# def dfs(first):
+#     if len(ans) == m:
 
+#         print(" ".join(map(str, ans)))
+#         return
+#     if first == n:
+#         return
+#     for i in range(1, n+1):
+#         ans.append(i)
+#         dfs(first+1)
+#         ans.pop()
+
+# n, m = map(int, input().split())
+# num_list = list(range(1,n+1))
+
+# dfs(0)
+
+
+### 백트래킹 연습
+
+# 리스트를 고대로 넘겨준다. 
+# def dfs(index, temp_list):
+#     if index == m:
+#         ans.append(temp_list)
+#         return
+#     for i in range(1, n+1):
+#         dfs(index+1, temp_list+[i])
+
+# n, m = map(int, input().split())
+# ans = []
+
+# dfs(0, [])
+# for j in ans:
+#     print(*j)
+
+# 리스트를 넘겨주지 않고 ans에 그대로 append 한다
+
+def dfs(index):
+    if index == m:
         print(" ".join(map(str, ans)))
-        return
-    if first == n:
         return
     for i in range(1, n+1):
         ans.append(i)
-        dfs(first+1)
+        dfs(index+1)
         ans.pop()
-
 n, m = map(int, input().split())
-num_list = list(range(1,n+1))
-
+ans = []
 dfs(0)
