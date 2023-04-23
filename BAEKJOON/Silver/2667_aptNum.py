79163a31357952361ef86cd8ddfaa82a5e2bb4d3
hellosonic-r <hellommmh@gmail.com>
2023-04-23 15:25:54 +0900

    baekjoon/silver1

--- BAEKJOON/Silver/2667_aptNum.py
+++ BAEKJOON/Silver/2667_aptNum.py
@@ -1,4 +1,4 @@
-# ## dfs 내 풀이.. 왜 틀린지 모르겠음
+# # ## dfs 내 풀이.. 왜 틀린지 모르겠음
 
 # def dfs(x,y):
 #     global count
@@ -15,30 +15,30 @@
 #                 count += 1
 #                 dfs(nx, ny)
 
-# n = int(input())
-# board = []
-# for _ in range(n):
-#     board.append(list(map(int, str(input()))))
-# dx = [0,1,0,-1]
-# dy = [1,0,-1,0]
-# v = [[0] * n for _ in range(n)]
-# count = 0
-# ans = []
+# # n = int(input())
+# # board = []
+# # for _ in range(n):
+# #     board.append(list(map(int, str(input()))))
+# # dx = [0,1,0,-1]
+# # dy = [1,0,-1,0]
+# # v = [[0] * n for _ in range(n)]
+# # count = 0
+# # ans = []
 
-# for j in range(n):
-#     for i in range(n):
-#         if board[j][i] == 1 and v[j][i] == 0:
-#             dfs(i,j)
-#             ans.append(count)
-#             count = 0
+# # for j in range(n):
+# #     for i in range(n):
+# #         if board[j][i] == 1 and v[j][i] == 0:
+# #             dfs(i,j)
+# #             ans.append(count)
+# #             count = 0
 
-# ans.sort()
-# print(len(ans))
-# for i in ans:
-#     print(i)
+# # ans.sort()
+# # print(len(ans))
+# # for i in ans:
+# #     print(i)
 
 
-# ## 이렇게 하니까 맞음 ..;;
+# # ## 이렇게 하니까 맞음 ..;;
 # def dfs(x,y):
 #     global count
 #     v[y][x] = 1
@@ -76,9 +76,9 @@
 
 
 
-# ## 그래.. 그럼 내 코드 뜯어고쳐보자 >> 맞았음. 
-# ## for문 안에서 count를 해주었던게 에러였던듯
-# ## dfs 호출 되었을 초기에 count 해주니까 맞음
+# # ## 그래.. 그럼 내 코드 뜯어고쳐보자 >> 맞았음. 
+# # ## for문 안에서 count를 해주었던게 에러였던듯
+# # ## dfs 호출 되었을 초기에 count 해주니까 맞음
 
 # def dfs(x,y):
 #     global count
@@ -118,45 +118,95 @@
 #     print(i)   
 
 
+# ##다시풀어보기
+# def dfs(x,y):
+#     global count
+#     board[y][x] = 0
+#     count += 1 #호출 되었을 때 카운트
+
+#     for i in range(4):
+#         nx = x+dx[i]
+#         ny = y+dy[i]
+#         if nx<0 or ny<0 or nx>=n or ny>=n:
+#             continue
+#         else:
+#             #호출 가능한 조건
+#             if board[ny][nx] == 1 and visited[ny][nx] == 0:
+#                 dfs(nx, ny)
+
+
+# n = int(input())
+# board = []
+# for _ in range(n):
+#     board.append(list(map(int, str(input()))))
+# visited = [[0]*n for _ in range(n)]
+# count = 0
+# ans = []
+
+# dx = [0,1,0,-1]
+# dy = [1,0,-1,0]
+
+# for j in range(n):
+#     for i in range(n):
+#         if board[j][i] == 1:
+#             dfs(i,j)
+#             ans.append(count)
+#             count = 0
+
+# ans.sort()
+
+# print(len(ans))
+# for i in ans:
+#     print(i)
+
+
 ##다시풀어보기
-def dfs(x,y):
-    global count
-    board[y][x] = 0
-    count += 1 
+from collections import deque
 
-    for i in range(4):
-        nx = x+dx[i]
-        ny = y+dy[i]
-        if nx<0 or ny<0 or nx>=n or ny>=n:
-            continue
-        else:
-            if board[ny][nx] == 1 and visited[ny][nx] == 0:
-                dfs(nx, ny)
-
+def bfs(sx,sy):
+    global num, count
+    queue = deque()
+    queue.append((sx,sy))
+    visited[sy][sx] = num
+    board[sy][sx] = 0
+    count = 1
+    
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx<0 or ny<0 or nx>=n or ny>=n:
+                continue
+            else:
+                if board[ny][nx] == 1 and visited[ny][nx] == 0:
+                    visited[ny][nx] = num
+                    board[ny][nx] = 0
+                    queue.append((nx,ny))
+                    count += 1
 
 n = int(input())
 board = []
 for _ in range(n):
     board.append(list(map(int, str(input()))))
-visited = [[0]*n for _ in range(n)]
-count = 0
-ans = []
 
 dx = [0,1,0,-1]
-dy = [1,0,-1,0]
+dy = [-1,0,1,0]
 
-for j in range(n):
-    for i in range(n):
-        if board[j][i] == 1:
-            dfs(i,j)
-            ans.append(count)
+visited = [[0]*n for _ in range(n)]
+num = 0
+count = 0
+count_list = []
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 1:
+            num+=1
+            bfs(j,i)
+            count_list.append(count)
             count = 0
 
-ans.sort()
-
-print(len(ans))
-for i in ans:
+print(num)
+for i in count_list:
     print(i)
-
-
+print(visited)
 
