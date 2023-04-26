0124cc529c219d35841ba4603617d5ec46eb8443
hellosonic-r <hellommmh@gmail.com>
2023-04-26 11:28:00 +0900

    baekjoon/silver2

---
+++ BAEKJOON/Silver/11725_parentNode.py
@@ -0,0 +1,20 @@
+def dfs(sn):
+    global ans
+    for i in board[sn]:
+        if visited[i] == 0:
+            visited[i] = sn
+            dfs(i)
+
+
+n = int(input())
+board = [[] for _ in range(n+1)]
+for _ in range(n-1):
+    s,e = map(int, input().split())
+    board[s].append(e)
+    board[e].append(s)
+
+visited = [0]*(n+1)
+dfs(1)
+for start_node in range(2,n+1):
+    print(visited[start_node])
+
