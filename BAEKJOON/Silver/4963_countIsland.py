da5fac2e84d00f14ad4ce53e142a8887c3c932a3
hellosonic-r <hellommmh@gmail.com>
2023-04-26 09:52:07 +0900

    baekjoon/silver2

--- BAEKJOON/Silver/4963_countIsland.py
+++ BAEKJOON/Silver/4963_countIsland.py
@@ -1,38 +1,38 @@
 # ##DFS-런타임 에러(recursionError였지만 setrecursionlimit로 해결)
-# import sys
-# sys.setrecursionlimit(10**6)
+import sys
+sys.setrecursionlimit(10**6)
 
-# def dfs(sx,sy):
-#     board[sy][sx] = 0
-#     visited[sy][sx] = 1
-#     if sx >= 50 or sy >= 50:
-#         return
-#     for i in range(8):
-#         nx = sx + dx[i]
-#         ny = sy + dy[i]
-#         if nx<0 or ny<0 or nx>=w or ny>=h:
-#             continue
-#         else:
-#             if visited[ny][nx] == 0 and board[ny][nx] == 1:
-#                 dfs(nx,ny)
+def dfs(sx,sy):
+    board[sy][sx] = 0
+    visited[sy][sx] = 1
+    if sx >= 50 or sy >= 50:
+        return
+    for i in range(8):
+        nx = sx + dx[i]
+        ny = sy + dy[i]
+        if nx<0 or ny<0 or nx>=w or ny>=h:
+            continue
+        else:
+            if visited[ny][nx] == 0 and board[ny][nx] == 1:
+                dfs(nx,ny)
 
-# while True:
-#     w, h = map(int,input().split())
-#     if w == 0 and h == 0:
-#         break
-#     board = []
-#     for _ in range(h):
-#         board.append(list(map(int, input().split())))
-#     dx = [0,1,0,-1,1,1,-1,-1]
-#     dy = [-1,0,1,0,-1,1,1,-1]
-#     visited = [[0]*w for _ in range(h)]
-#     count = 0
-#     for i in range(h):
-#         for j in range(w):
-#             if board[i][j] == 1:
-#                 dfs(j,i)
-#                 count += 1
-#     print(count)
+while True:
+    w, h = map(int,input().split())
+    if w == 0 and h == 0:
+        break
+    board = []
+    for _ in range(h):
+        board.append(list(map(int, input().split())))
+    dx = [0,1,0,-1,1,1,-1,-1]
+    dy = [-1,0,1,0,-1,1,1,-1]
+    visited = [[0]*w for _ in range(h)]
+    count = 0
+    for i in range(h):
+        for j in range(w):
+            if board[i][j] == 1:
+                dfs(j,i)
+                count += 1
+    print(count)
 
 ##BFS
 from collections import deque
