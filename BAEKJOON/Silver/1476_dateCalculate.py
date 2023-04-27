c2bf639af9e96eedd71247fdcba5ecd70d503819
hellosonic-r <hellommmh@gmail.com>
2023-04-27 13:17:43 +0900

    baekjoon/silver5

--- BAEKJOON/Silver/1476_dateCalculate.py
+++ BAEKJOON/Silver/1476_dateCalculate.py
@@ -1,3 +1,4 @@
+##백트래킹
 import sys
 sys.setrecursionlimit(10**6)
 
@@ -17,4 +18,26 @@ def dfs(x,y,z, count):
 e,s,m = map(int, input().split())
 ans = 0
 dfs(1,1,1,1)
+print(ans)
+
+
+##단순 반복문
+e,s,m = map(int, input().split())
+
+a,b,c = 1,1,1
+ans = 1
+while True:
+    if a == e and b == s and c == m:
+        break
+    a += 1
+    b += 1
+    c += 1
+    if a == 16:
+        a = 1
+    if b == 29:
+        b = 1
+    if c == 20:
+        c = 1
+    ans += 1
+
 print(ans)
