ce333d441b9599efd246ec565368a671744be236
hellosonic-r <hellommmh@gmail.com>
2023-05-16 16:32:39 +0900

    SWEA/D3

---
+++ SWEA/D3/4299_loveTiming.py
@@ -0,0 +1,20 @@
+t = int(input())
+
+for test_case in range(1, t+1):
+    d,h,m = map(int, input().split())
+    ans = ((d-11) * 24 * 60) + (h-11)*60 + (m-11)
+    if ans < 0:
+        ans = -1
+    print("#{} {}".format(test_case, ans))
+
+
+##다시 풀기
+t = int(input())
+for test_case in range(1, t+1):
+    d, h, m = map(int, input().split())
+    ans = (d*24*60 + h*60 + m) - (11*24*60 + 11*60 + 11)
+
+    if ans < 0:
+        ans = -1
+
+    print("#{} {}".format(test_case, ans)) 
