cd32685edbb8f803712cac4f9820ce90f2acbcda
hellosonic-r <hellommmh@gmail.com>
2023-05-28 13:14:24 +0900

    baekjoon/bronze1

---
+++ BAEKJOON/Bronze/10989_sort3.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(input())
+arr = [0] * 10001
+
+for i in range(n):
+    num = int(sys.stdin.readline())
+    arr[num] += 1
+
+for i in range(1, 10001):
+    if arr[i] != 0:
+        for j in range(arr[i]):
+            print(i)
+
