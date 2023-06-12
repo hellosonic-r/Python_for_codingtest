9c198bc405615a6eee17ee9bd401cc7682cef40d
hellosonic-r <hellommmh@gmail.com>
2023-06-12 19:50:54 +0900

    baekjoon/silver2

---
+++ BAEKJOON/Silver/2805_cutTree.py
@@ -0,0 +1,48 @@
+##나의 코드
+n, m = map(int, input().split()) #n:나무의 수 m:목표나무의길이
+tree = list(map(int, input().split()))
+tree.sort() # 10 15 17 20
+
+result = []
+## 절단기의 높이를 0~나무의높이의 최대값으로 지정할 수 있음(즉, 0~20)
+start = 0 #절단기의 높이의 시작값:0
+end = tree[-1] #절단기의 높이의 끝값: 나무의높이의 최대값
+ 
+while start <= end: 
+    mid = (start + end) // 2 #절단기의 높이를 중앙값부터 설정
+    cutting = 0 #잘랐을 때의 나무의 길이
+    for t in tree: #나무의 높이를 저장
+        if t >= mid: #설정한 절단기의 높이보다 나무가 크다면 자를 수 있으므로
+            cutting += (t-mid) #잘랐을때의 나무의 길이의 총합에 저장
+    if cutting >= m: #만약 나무의 길이의 총합이 목표값 이상이라면
+        result.append(mid) #결과 리스트에 저장
+    if cutting > m: #만약 목표값보다 길게 잘렸다면
+        start = mid + 1 #절단기의 높이를 올린다.
+    elif cutting < m: #만약 목표값보다 짧게 잘렸다면
+        end = mid -1 #절단기의 높이를 낮춘다
+    else:
+        break
+
+print(max(result))
+    
+
+#다른 코드(더 깔끔)
+n, m = map(int, input().split())
+tree = list(map(int, input().split()))
+start, end = 1, max(tree)
+
+while start <= end:
+    mid = (start+end)//2
+    log = 0 #잘린 나무의 총합
+    for t in tree:
+        if t >= mid:
+            log += (t-mid)
+
+    if log >= m:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+#start > end가 되게 되면 반복문을 탈출하게 되는데,
+#이때 end에 저장된 값이 찾으려는 값과 동일하다.
+print(end) 
